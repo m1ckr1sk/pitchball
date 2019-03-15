ade78518006b251f5d43ff8ef36665186290838e
mike.butt <mike.butt@os.uk>
2019-03-15 00:13:29 +0000

    Corrected passes being counted as attacks

--- pitchball/player_strategies/player_strategy.py
+++ pitchball/player_strategies/player_strategy.py
@@ -12,5 +12,8 @@ class PlayerStrategy:
     def __str__(self):
         pass
 
-    def return_success(self, defender, attacker):
-        return random.randint(1, 2 + max(defender.ability["defend"] - attacker.ability["attack"] // 2, 0))
+    def return_success(self, to_player, from_player):
+        if to_player.team == from_player.team:
+            return random.randint(1, 2 + to_player.ability["defend"] + from_player.ability["passing"] // 2)
+        else:
+            return random.randint(1, 2 + max(to_player.ability["defend"] - from_player.ability["attack"] // 2, 0))
